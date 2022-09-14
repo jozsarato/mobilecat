ee266e3152a1ba9bea28945163dfd58055d66520
jozsarato <jozsef.arato@univie.ac.at>
2022-09-14 15:08:52 +0200

    file structure update

---
+++ setup.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 14 15:06:19 2022
+
+@author: aratoj87
+"""
+
+
+from setuptools import find_packages, setup
+
+setup(
+name='mobileeyecat',
+author='jozsef arato',
+version='0.1',
+packages=find_packages(),)
