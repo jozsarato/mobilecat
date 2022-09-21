fd68bb2d4fde0e3760fe71e4f5dda5259751fd97
jozsarato <jozsef.arato@univie.ac.at>
2022-09-21 16:31:16 +0200

    model fit and prediciton functions added

--- mobilecat/classmodels.py
+++ mobilecat/classmodels.py
@@ -14,6 +14,12 @@ from tensorflow.keras.preprocessing.image import img_to_array
 import sys
 import os
 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten
+from tensorflow.keras import utils
+
+# from tensorflow import keras
 
 
 def get_immediate_subdirectories(a_dir):
@@ -22,14 +28,16 @@ def get_immediate_subdirectories(a_dir):
 
 
 def GetDirs(path):
+    ''' get subdirectories from path '''
     Dirs=get_immediate_subdirectories(path)
     print('directories ', Dirs)
     return np.sort(Dirs)
        
 
-def TrainImagestoNumpy(path,Dirs,ToSave=0,Mac=0,  Setup=1,Dim=96):
-    Numfiles=np.zeros(9)
+def TrainImagestoNumpy(path,Dirs,ToSave=0,Mac=0,  Setup=1,Dim=96,NCat=9):
+    ''' load all image files from subdirectories and save as  comined numpy array, for each subfolder, excpects separte folder for each category'''
 
+    Numfiles=np.zeros(NCat)
     assert len(Dirs)==len(Numfiles), f'file number folder number mismatch, got {len(Dirs)}'
     for cd,d in enumerate(Dirs):
         if Mac==1:
@@ -59,6 +67,8 @@ def TrainImagestoNumpy(path,Dirs,ToSave=0,Mac=0,  Setup=1,Dim=96):
 
 
 def TestImagestoNumpy(path,Dirs,Dim=96,ToSave=0,Mac=0,  Setup=1):
+    ''' load all image files from subdirectories and save as  comined numpy array for each subfolder'''
+
     numfiles=np.zeros(len(Dirs))
     print('directories ', Dirs)
     for cd,d in enumerate(Dirs):
@@ -75,9 +85,6 @@ def TestImagestoNumpy(path,Dirs,Dim=96,ToSave=0,Mac=0,  Setup=1):
                     Image=plt.imread(path+'/'+d+'/'+f)
                 else:
                     Image=plt.imread(path+'\\'+d+'/'+f)
-                if Setup==1:
-                    Image=cv2.cvtColor(Image, cv2.COLOR_BGR2RGB)  
-                #image = load_img(path+'\\'+d+'/'+f)
                 data = img_to_array(Image)
                 ImageArray[cf,:,:,:]=data
             np.save('image_'+str(d)+'_test',ImageArray)
@@ -92,14 +99,11 @@ def TestImagestoNumpy(path,Dirs,Dim=96,ToSave=0,Mac=0,  Setup=1):
 #     path='Pilot2Selected'
     
     
-    
-NCat=9   # number of stim categories 
+   
+def TrainTestSel(NCat,Dirs,NumTrain,NumTest,NumFiles,Dim,visN=3):
+    ''' load numpy array for each category and randomly select training and test set,
+    optionally visN examples are visualized from both training and test set '''
 
-
-
-def TrainTestSel(NCat,Dirs,NumTrain,NumTest,NumFiles,Dim):
-    
-    visN=3
     if len(Dirs)!=NCat:
         print(f"WARNING mismatch directory and NCat, first {NCat}. dirs are used")
     TrainX=np.int16(np.zeros((((NumTrain*NCat,Dim,Dim,3)))))
@@ -108,7 +112,8 @@ def TrainTestSel(NCat,Dirs,NumTrain,NumTest,NumFiles,Dim):
     TestY=np.zeros(NumTest*NCat)    
     assert np.min(NumFiles)> NumTrain+NumTest, f"not enough files in at least one for folder, min {NumTrain+NumTest} needed"
     for cd,d in enumerate(Dirs[0:NCat]):
-        fig,ax=plt.subplots(nrows=3,ncols=2)
+        if visN:
+            fig,ax=plt.subplots(nrows=visN,ncols=2)
         print(cd,d,'Num images',int(NumFiles[cd]))
         ImageArrayL=np.load('image_'+str(d)+'.npy')
         Rand=np.intp(np.random.permutation(np.arange(NumFiles[cd])))
@@ -128,7 +133,52 @@ def TrainTestSel(NCat,Dirs,NumTrain,NumTest,NumFiles,Dim):
             for h in range(2):
                 ax[v,h].set_xticks([])
                 ax[v,h].set_yticks([])
-            
-        plt.tight_layout()
+        if visN:    
+            plt.tight_layout()
         
     return TrainX,TestX,TrainY,TestY
+
+
+
+def MakeCat(TrainY,TestY):
+    return utils.to_categorical(TrainY),utils.to_categorical(TestY)
+
+
+
+def CNN2hidden(dims_train,numcat):
+    '''  set up keras convolutional neural network with two hidden layers '''
+    model_CNN = Sequential()  
+    model_CNN.add(Conv2D(filters=32, kernel_size=5, padding='same', activation='relu', input_shape=(dims_train[1],dims_train[2], dims_train[3]))) 
+    model_CNN.add(MaxPooling2D(pool_size=2))
+    model_CNN.add(Conv2D(filters=64, kernel_size=3, padding='same', activation='relu')) 
+    model_CNN.add(MaxPooling2D(pool_size=2))
+    model_CNN.add(Flatten())
+    model_CNN.add(Dense(numcat, activation='softmax'))
+    model_CNN.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    
+    return model_CNN
+
+
+def fitMod(model,X,testx, Y,testy,nepochs=1):
+    'fit keras model '
+    model.fit(x=X,y=Y, epochs=nepochs, validation_data=(testx,testy))
+    return model
+
+
+def ModPred(model,X):
+    ''' make predictions with fitted model '''
+    preds=model.predict(X)
+    return np.argmax(preds,1)
+
+
+def GetAccuracies(preds,trueY,NCat):
+    ''' get prediciton accuracy for each category '''
+    accurs=np.zeros(NCat)    
+    Ns=np.zeros(NCat)
+    for i in range(NCat):
+        accurs[i]=np.sum(preds[trueY==i]==i)
+        Ns[i]=np.sum(trueY==i)
+    return accurs/Ns
+
+
